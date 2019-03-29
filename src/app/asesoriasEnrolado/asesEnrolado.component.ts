191222c4599bc5e2be29a6c65a5fa1d6babb3ee8
FerRojas117 <fercho117_25halo@hotmail.com>
2019-03-29 17:38:12 -0600

    first commit

---
+++ src/app/asesoriasEnrolado/asesEnrolado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  templateUrl: "./asesEnrolado.component.html",
+
+})
+export class AsesEnroladoComponent {
+
+
+  constructor() {}
+
+
+}
